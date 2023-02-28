8f06b21b31ece1238ee476a6b70ea68abd3d6c7c
David Kouřil <david.kouril@gmail.com>
2023-02-28 14:19:05 +0100

    selection widget prototype locally

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,59 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import * as d3 from "d3";
+  import { onMount } from "svelte";
+
+  const width = 400;
+  const height = 400;
+
+  const SelectionWidget = (num = 100) => {
+    const svg = d3
+      .create("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .style("background-color", "#aaaaaa")
+      .attr("viewBox", [-width / 2, -height / 2, width, height]);
+
+    const innerRadius = 160;
+    const outerRadius = 200;
+
+    const elNum = num;
+    const V = d3.range(elNum);
+    const N = d3.range(elNum);
+    const I = d3.range(N.length).filter((i) => !isNaN(V[i]));
+
+    const arcs = d3
+      .pie()
+      .padAngle(1 / outerRadius)
+      .sort(null)
+      .value(1)(I);
+    const arc = d3.arc().innerRadius(innerRadius).outerRadius(outerRadius);
+
+    svg
+      .append("g")
+      .attr("stroke", "white")
+      .attr("stroke-width", 1)
+      .attr("stroke-linejoin", "round")
+      .selectAll("path")
+      .data(arcs)
+      .join("path")
+      .attr("fill", "#bbbbbb")
+      .attr("d", arc);
+
+    return svg.node();
+  };
+
+  onMount(() => {
+    const container = d3.select("#container").node();
+    container.appendChild(SelectionWidget(300));
+    container.appendChild(SelectionWidget(100));
+    container.appendChild(SelectionWidget(50));
+    //d3.select("#container").append("p");
+  });
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<div id="container" />
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+<main />
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
