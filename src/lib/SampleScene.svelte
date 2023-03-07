5c54533c87866e724d24fb475e8f076e6ad04ca9
David Kouřil <david.kouril@gmail.com>
2023-03-07 14:50:00 +0100

    playing with scale

--- src/lib/SampleScene.svelte
+++ src/lib/SampleScene.svelte
@@ -3,38 +3,43 @@
     import { onMount } from "svelte";
     import { spring } from "svelte/motion";
     import { degToRad } from "three/src/math/MathUtils";
-    import {parsePdb} from "./pdb";
+    import { parsePdb } from "./pdb";
     import { brafl } from "./test_BRAFL";
 
     // const scale = spring(1);
+    // const scale = 0.02;
     const scale = 0.02;
+    const radiusScale = 0.2;
 
-    let spheres = [{x: 0, y: 0, z: 0}];
+    let spheres = [{ x: 0, y: 0, z: 0 }];
 
-    const randomPositions = (num: number): {x: number, y: number, z: number}[] => {
+    const randomPositions = (
+        num: number
+    ): { x: number; y: number; z: number }[] => {
         let newSpheresArr = [];
         const max = 50;
         for (let i = 0; i < num; i++) {
             const x = Math.random() * max;
             const y = Math.random() * max;
             const z = Math.random() * max;
-            newSpheresArr.push({x: x, y: y, z: z});
+            newSpheresArr.push({ x: x, y: y, z: z });
         }
         return newSpheresArr;
-    }
+    };
 
     onMount(() => {
         // spheres = randomPositions(100);
-        spheres = parsePdb(brafl).bins;
+        // spheres = parsePdb(brafl).bins;
+        spheres = parsePdb(brafl).bins.map(({x , y, z}) => ({x: x*scale, y: y*scale, z: z*scale}));
     });
 </script>
 
 <div style="width: 600px; height: 400px;">
     <Canvas>
-        <T.PerspectiveCamera makeDefault position={[10, 10, 10]} fov={24}>
+        <T.PerspectiveCamera makeDefault position={[10, 10, 20]} fov={24}>
             <OrbitControls
-                maxPolarAngle={degToRad(80)}
-                enableZoom={false}
+                maxPolarAngle={degToRad(90)}
+                enableZoom={true}
                 target={{ y: 0.5 }}
             />
         </T.PerspectiveCamera>
@@ -43,28 +48,18 @@
         <T.DirectionalLight position={[-3, 10, -10]} intensity={0.2} />
         <T.AmbientLight intensity={0.2} />
 
-        <!-- Cube -->
-        <!-- <T.Group scale={$scale}> -->
-        <T.Group scale={scale}>
+        <T.Group>
             {#each spheres as s}
                 <T.Mesh
                     position.y={s.y}
                     position.x={s.x}
                     position.z={s.z}
+                    scale={radiusScale}
                     castShadow
                     let:ref
                 >
-                    <!-- Add interaction -->
-                    <!-- <InteractiveObject
-                        object={ref}
-                        interactive
-                        on:pointerenter={() => ($scale = 0.2)}
-                        on:pointerleave={() => ($scale = 0.1)}
-                    /> -->
-
-                    <!-- <T.BoxGeometry /> -->
                     <T.SphereGeometry />
-                    <T.MeshStandardMaterial color="#333333" />
+                    <T.MeshStandardMaterial color="#aaaaaa" />
                 </T.Mesh>
             {/each}
         </T.Group>
@@ -72,7 +67,7 @@
         <!-- Floor -->
         <T.Mesh receiveShadow rotation.x={degToRad(-90)}>
             <T.CircleGeometry args={[3, 72]} />
-            <T.MeshStandardMaterial color="white" />
+            <T.MeshStandardMaterial color="#333333" />
         </T.Mesh>
     </Canvas>
 </div>
