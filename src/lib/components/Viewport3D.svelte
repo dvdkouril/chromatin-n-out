d8af2644f2c21cacfb45e5b408cee5b9bf26949f
David Kouril <david.kouril@gmail.com>
2023-04-06 08:42:39 +0200

    viewport

--- src/lib/components/Viewport3D.svelte
+++ src/lib/components/Viewport3D.svelte
@@ -1,18 +1,164 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    import * as THREE from "three";
-
+    import { getContext, onMount, setContext } from "svelte";
+    import { writable, type Writable } from "svelte/store";
+    import type * as Graphics from "../../lib-graphics";
+  
+    // Context
+    let device: Writable<GPUDevice> = getContext("device");
+    let graphicsLibrary: Writable<Graphics.GraphicsLibrary> =
+      getContext("graphicsLibrary");
+  
+    let context: GPUCanvasContext | null = null;
+    let viewportInner: Writable<Graphics.Viewport3D | null> = writable(null);
+    
+    export let viewport = $viewportInner;
+  
+    $: if ($viewportInner) {
+      viewport = $viewportInner;
+    }
+  
+    setContext("viewport", viewportInner);
+  
+    $: {
+      if ($viewportInner) {
+        $viewportInner.deallocate();
+      }
+  
+      if ($graphicsLibrary) {
+        $viewportInner = $graphicsLibrary.create3DViewport();
+      }
+    }
+  
+    let wrapper: HTMLDivElement;
+    let canvas: HTMLCanvasElement;
+  
+    let width = 0;
+    let height = 0;
+  
+    $: style =
+      "width:" +
+      (width / window.devicePixelRatio).toString() +
+      "px; height:" +
+      (height / window.devicePixelRatio).toString() +
+      "px";
+  
+    async function render(t: number) {
+      if (!$viewportInner || !context) {
+        return;
+      }
+  
+      if (width <= 0 || height <= 0) {
+        return;
+      }
+  
+      const texture = context.getCurrentTexture();
+      const view = texture.createView();
+  
+      await $viewportInner.render(view, t);
+    }
+  
+    function canvasOnMouseDown(event: MouseEvent) {
+      if (!$viewportInner) return;
+  
+      $viewportInner.camera.onMouseDown(event);
+    }
+  
+    function canvasOnMouseUp(event: MouseEvent) {
+      if (!$viewportInner) return;
+  
+      $viewportInner.camera.onMouseUp(event);
+    }
+  
+    function canvasOnMouseOut(event: MouseEvent) {}
+  
+    function canvasOnMouseMove(event: MouseEvent) {
+      if (!$viewportInner) return;
+  
+      $viewportInner.camera.onMouseMove(event);
+    }
+  
+    function canvasOnScroll(event: WheelEvent) {
+      if (!$viewportInner) return;
+  
+      $viewportInner.camera.onWheelEvent(event);
+    }
+  
     onMount(() => {
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(
-            75,
-            window.innerWidth / window.innerHeight,
-            0.1,
-            1000
-        );
-
-        const renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(renderer.domElement);
+      const resizeObserver = new ResizeObserver((entries) => {
+        const entry = entries.at(0);
+  
+        if (
+          entry instanceof ResizeObserverEntry &&
+          entry.devicePixelContentBoxSize
+        ) {
+          width = entry.devicePixelContentBoxSize[0].inlineSize;
+          height = entry.devicePixelContentBoxSize[0].blockSize;
+  
+          if ($viewportInner) {
+            $viewportInner.resize(width, height);
+          }
+        }
+      });
+  
+      resizeObserver.observe(wrapper, { box: "device-pixel-content-box" });
+  
+      context = canvas.getContext("webgpu");
+      context.configure({
+        device: $device,
+        format: navigator.gpu.getPreferredCanvasFormat(),
+      });
+  
+      let frame: number;
+      const loop = async (frametime: number) => {
+        if (context && $device) {
+          await render(frametime);
+        }
+  
+        frame = requestAnimationFrame(loop);
+      };
+      frame = requestAnimationFrame(loop);
+  
+      return () => {
+        resizeObserver.unobserve(canvas);
+        cancelAnimationFrame(frame);
+  
+        if ($viewportInner) {
+          $viewportInner.deallocate();
+        }
+      };
     });
-</script>
+  </script>
+  
+  <div bind:this={wrapper} class="wrapper">
+    <canvas
+      bind:this={canvas}
+      {width}
+      {height}
+      {style}
+      on:mousedown={canvasOnMouseDown}
+      on:mousemove={canvasOnMouseMove}
+      on:mouseup={canvasOnMouseUp}
+      on:mouseleave={canvasOnMouseOut}
+      on:wheel={canvasOnScroll}
+    />
+    {#if $device && $viewportInner}
+      <slot />
+    {/if}
+  </div>
+  
+  <style>
+    .wrapper {
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      position: absolute;
+    }
+  
+    canvas {
+      width: 100%;
+      height: 100%;
+      position: relative;
+    }
+  </style>
+  
