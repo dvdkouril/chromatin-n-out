717281d593704d398a08b15d91ec813547eb5ff5
David Kouřil <david.kouril@gmail.com>
2023-04-27 16:00:49 +0200

    separate prototypes

--- src/App.svelte
+++ src/App.svelte
@@ -1,98 +1,29 @@
 <script lang="ts">
-  import {
-    generateColors,
-    generateGrayScale,
-    generateNicerColors,
-  } from "./lib/util";
-  import { onMount } from "svelte";
-  import { parsePdb } from "./lib/pdb";
-  import { brafl } from "./lib/test_BRAFL";
-  import { cell7 } from "./lib/test_cell7";
-  import ColumnsHierarchicalLayout from "./lib/components/ColumnsHierarchicalLayout.svelte";
-  import type { Widget } from "./lib/widget";
-    import BubblesPrototype from "./lib/components/BubblesPrototype.svelte";
+  import BubblesPrototype from "./lib/components/BubblesPrototype.svelte";
+  import HierarchicalSelectionsPrototype from "./lib/prototypes/HierarchicalSelectionsPrototype.svelte";
 
-  const hyperWindowSize = 250;
-  const selectionWidgetThickness = 25;
-  // $: colorMap = generateColors(topLevelBinsNum);
-  // $: grayColorMap = generateGrayScale(topLevelBinsNum);
-  // $: nicerColorMap = generateNicerColors(topLevelBinsNum);
-  $: selectionsColormap = generateNicerColors(topLevelBinsNum);
-  let nextAvailableId = 1; //~ 0 is hardcoded onMount
-
-  let widgetTreeRoot: Widget = null;
-  let maxLevel: number = 0;
-
-  //~ 3D data
-  const scale = 0.02;
-  let spheres = [];
-  let topLevelBinsNum = 0;
-
-  const newSelection = (ev) => {
-    console.log("App: seeing change");
-    console.log(ev);
-    const sel = ev.detail.selection;
-    const sourceWidget = ev.detail.sourceWidget;
-    const offset = sourceWidget.domain.start;
-
-    const newWidgetId = nextAvailableId;
-    nextAvailableId += 1;
-
-    const changedLevel = sourceWidget.level + 1;
-    if (changedLevel > maxLevel) maxLevel = changedLevel;
-    const newWidget = {
-      id: newWidgetId,
-      level: changedLevel,
-      binsNum: sel.end - sel.start,
-      domain: { start: offset + sel.start, end: offset + sel.end },
-      selections: [],
-      colorForSelections: sel.color,
-      widgets: [],
-    };
-
-    sourceWidget.widgets.push(newWidget);
-    widgetTreeRoot = widgetTreeRoot; //~ because...reactivity
-    console.log("current hierarchy:");
-    console.log(widgetTreeRoot);
-  };
-
-  onMount(() => {
-    console.log("onMount");
-
-    //~ load the PDB
-    spheres = parsePdb(brafl).bins.map(({ x, y, z }) => ({
-    // spheres = parsePdb(cell7).bins.map(({ x, y, z }) => ({
-      x: x * scale,
-      y: y * scale,
-      z: z * scale,
-    }));
-    topLevelBinsNum = spheres.length;
-    console.log("num of spheres = " + topLevelBinsNum);
-
-    const rootWidget = {
-      id: 0,
-      level: 0,
-      binsNum: topLevelBinsNum,
-      domain: {
-        start: 0,
-        end: topLevelBinsNum - 1,
-      },
-      selections: [],
-      colorForSelections: null,
-      widgets: [],
-    };
-
-    widgetTreeRoot = rootWidget;
-    maxLevel = 0;
-    console.log("onMount finished");
-  });
+  let prototypes = ["hierarchical-selections", "bubbles"];
+  let currentPrototype = "hierarchical-selections";
 </script>
 
-<ColumnsHierarchicalLayout {widgetTreeRoot} {maxLevel} {hyperWindowSize} {selectionWidgetThickness} newSelectionCallback={newSelection} {selectionsColormap} {spheres} />
+<div id="prototypes-menu">
+  <button
+    on:click={() => {
+      currentPrototype = prototypes[0];
+    }}>hierarchical selections</button
+  >
+  <button
+    on:click={() => {
+      currentPrototype = prototypes[1];
+    }}>bubbles</button
+  >
+</div>
 
-<!-- <ForceTest /> -->
-<BubblesPrototype />
-<main />
+{#if currentPrototype == "hierarchical-selections"}
+  <HierarchicalSelectionsPrototype />
+{:else if currentPrototype == "bubbles"}
+  <BubblesPrototype />
+{/if}
 
 <style>
 </style>
