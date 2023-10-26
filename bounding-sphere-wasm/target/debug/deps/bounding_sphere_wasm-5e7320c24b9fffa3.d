e48f72416b80887e5a15b8f431b4d2b45820988e
David Kouřil <david.kouril@gmail.com>
2023-10-26 12:36:36 -0400

    init default rust project

---
+++ bounding-sphere-wasm/target/debug/deps/bounding_sphere_wasm-5e7320c24b9fffa3.d
@@ -0,0 +1,7 @@
+/Users/dvdkouril/projects/chromatin-n-out/bounding-sphere-wasm/target/debug/deps/bounding_sphere_wasm-5e7320c24b9fffa3.rmeta: src/lib.rs
+
+/Users/dvdkouril/projects/chromatin-n-out/bounding-sphere-wasm/target/debug/deps/libbounding_sphere_wasm-5e7320c24b9fffa3.rlib: src/lib.rs
+
+/Users/dvdkouril/projects/chromatin-n-out/bounding-sphere-wasm/target/debug/deps/bounding_sphere_wasm-5e7320c24b9fffa3.d: src/lib.rs
+
+src/lib.rs:
---
+++ bounding-sphere-wasm/target/debug/libbounding_sphere_wasm.d
@@ -0,0 +1,1 @@
+/Users/dvdkouril/projects/chromatin-n-out/bounding-sphere-wasm/target/debug/libbounding_sphere_wasm.rlib: /Users/dvdkouril/projects/chromatin-n-out/bounding-sphere-wasm/src/lib.rs
