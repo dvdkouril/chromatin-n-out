37e73f0d82b15c49744729f49c52090938b76123
David Kouril <david.kouril@gmail.com>
2024-01-03 14:13:13 -0500

    Add new route: naive-browser For implementing a basic 3d genome browser in order to compare to hyperwindows

---
+++ src/routes/naive-browser/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import App from '$lib/components/App.svelte'
+</script>
+
+<div>
+  <!-- <App /> -->
+  <span style="color: white">
+  TEST! Welcome to the Naive Browser prototype.
+  </span>
+</div>
+
+<style>
+  :global(body) {
+    margin: 0;
+  }
+
+  div {
+    width: 100vw;
+    height: 100vh;
+    background: rgb(13, 19, 32);
+    background: linear-gradient(180deg, rgba(13, 19, 32, 1) 0%, rgba(8, 12, 21, 1) 100%);
+  }
+</style>
